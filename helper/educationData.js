eb04f8d2be0af6d0acb932036064fb690687260d
Abida Parveen <abida.parveen840@gmail.com>
2023-08-27 15:29:15 +0500

    merge with helper

---
+++ helper/educationData.js
@@ -0,0 +1,24 @@
+const educationData = [
+  {
+    id: 1,
+    city: "Jamshoro, Pakistan",
+    duration: "Nov 2016 to Jan 2021",
+    academy: "Mehran University of Engineering and Technology",
+    degree: "BE Software Engineering",
+  },
+  {
+    id: 2,
+    city: "Hyderabad, Pakistan",
+    duration: "2014 to 2016",
+    academy: "Creative Kids High School and College",
+    degree: "Intermediate",
+  },
+  {
+    id: 3,
+    city: "Hyderabad, Pakistan",
+    duration: "2012 to 2014",
+    academy: "Modern English High School",
+    degree: "Matriculation",
+  },
+];
+export default educationData
