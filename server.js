837392965f592d7e152a768d4d75579a65087a71
Abida Parveen Abro <57529906+abida-parveen@users.noreply.github.com>
2024-07-08 00:06:46 +0500

    Updated domain name in server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app
     const whitelist = [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "http://abida.live",
+      "http://theabida.com",
     ];
     // var corsOptions = {
     //   credentials: true,
